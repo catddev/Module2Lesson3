2c3300c51588e3eea89abd0e80519d823e612d72
айкеновад <@234_11>
2018-05-31 09:02:37 +0600

    -

---
+++ Module2Lesson3/Module2Lesson3.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+
+void main()
+
+{
+	setlocale(LC_ALL, "Rus");
+
+	int v = 0;
+	printf("νη:\t");
+	scanf("%d", &v);
+
+	if (v == 1) {
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
